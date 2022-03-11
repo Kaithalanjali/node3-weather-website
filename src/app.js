4cda5747b669d059204e19e4ee1e0fb24066c5c2
unknown <anjalikaithal25@gmail.com>
2022-03-11 23:59:15 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { response } = require('express')
  console.log(__dirname)
 //  console.log(path.join(__dirname, '../public'))
  const app= express()
-
+const port=process.env.PORT || 3000
  //Define paths for Express config
 const publicDirectoryPath=path.join(__dirname, '../public')
 const viewsPath=path.join(__dirname,'../templates/views')
@@ -124,7 +124,7 @@ app.get('*',(req, res)=>{
 }) 
 //app.com/weather
 
- app.listen(3000,()=>{
-     console.log('Server is on port 3000')
+ app.listen(port,()=>{
+     console.log('Server is on port' + port)
     })
     //hbs(handle behind scene) is used to integrate handle bars with express
